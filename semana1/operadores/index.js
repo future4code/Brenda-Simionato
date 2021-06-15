e62a467fc65604459049ddb73e73f7186134fed5
Brenda Simionato <brendaevellyn97@gmail.com>
2021-06-15 12:37:56 -0300

    Exercícios - Aula 04

---
+++ semana1/operadores/index.js
@@ -0,0 +1,60 @@
+/*Exercícios de interpretação de código
+
+Exercício 1.
+Reposta: 
+A - false
+B - false
+C - True
+D - Boolean
+
+
+Exercício 2.
+Resposta: 
+Os números digitados serão concatenados, 
+pois ao utilzar a função prompt, eles se tornarão uma string,
+para utilizá-los como número seria necessário a função "Number" anteriormente.
+
+Exercício 3.
+Resposta:
+Converter para número usando a função Number.*/
+
+
+/*Exercícios de escrita de código
+
+Exercício 1.
+Resposta:
+const idade = Number(prompt("Qual é a sua idade?"))
+const idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo?"))
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", idade > idadeAmigo)
+console.log("Diferença de idades:", idade - idadeAmigo)
+
+Exercício 2.
+Resposta:*/
+/*const numeroPar = Number(prompt("Digite um número par"))
+console.log("O resto da divisão será:", numeroPar % 2)*/
+/*O retorno notado é que sempre será 0, pois, estamos divindo números pares.
+Se o usuário digitar o número ímpar, sempre será 1
+
+Exercício 3.
+Resposta:*/
+/*const idadeAnos = Number(prompt("Quantos anos você tem?"))
+console.log(idadeAnos)
+const idadeMeses = idadeAnos * 12
+console.log(idadeMeses)
+const idadeDias = idadeAnos * 365
+console.log(idadeDias)
+const idadeHoras = idadeAnos * (idadeDias * 24)
+console.log(idadeHoras)*/
+
+/*Exercício 4.
+Resposta:*/
+/*const primeiroNumero = Number(prompt("Digite o primeiro número"))
+const segundoNumero = Number(prompt("Digite o segundo número"))
+
+console.log("O primeiro número é maior que o segundo?", primeiroNumero > segundoNumero)
+console.log("O primeiro numero é igual ao segundo?", primeiroNumero == segundoNumero)
+console.log("O primeiro numero é divisível pelo segundo?", primeiroNumero % segundoNumero == 0)
+console.log("O segundo numero é divisível pelo primeiro?", segundoNumero % primeiroNumero == 0 )*/
+
+
