3cc6f91ecccd73701aa4571e89272bb0bd975132
Brenda Simionato <brendaevellyn97@gmail.com>
2021-06-16 13:25:18 -0300

    Exercícios - Aula 05

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,55 @@
+/*Exercícios de interpretação de código:
+
+Exercício 1.
+Resposta:
+Undefined
+Null
+11
+3
+[3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+9
+
+Exercício 2.
+Resposta:
+O que será impresso no console: SUBI NUM ÔNIBUS EM MIRROCOS 27
+Todas as letras maiúsculas devido ao toUpperCase()
+Substituiu as vogais A por I pelo comando replaceAll("A", "I")
+E length dirá o tamanho da string.
+
+Exercícios de escrita de código:
+
+Exercício 1.*/
+/*const nome = prompt('Qual o seu nome?')
+const email = prompt('Qual o seu e-mail?')
+
+console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vindo(a) ${nome} !`)
+
+Exercício 2.
+Resposta:*/
+
+/*const comidasPreferidas = ["Churrasco", "Lasanha", "Macarrão", "Pizza", "Hambúrguer"]
+console.log(comidasPreferidas)
+
+console.log("Essas são minhas comidas preferidas:")
+console.log(comidasPreferidas[0])
+console.log(comidasPreferidas[1])
+console.log(comidasPreferidas[2])
+console.log(comidasPreferidas[3])
+console.log(comidasPreferidas[4])
+
+const comidaUsuario = prompt("Qual sua comida preferida?")
+comidasPreferidas[1] = comidaUsuario
+console.log(comidasPreferidas)
+
+Exercício 3.
+Resposta:*/
+
+/*const listaDeTaredas = []
+listaDeTaredas[0] = prompt("Qual a sua primeira tarefa diária?")
+listaDeTaredas[1] = prompt("Qual a sua segunda tarefa do dia?")
+listaDeTaredas[2] = prompt("Qual sua terceira tarefa do dia?")
+console.log(listaDeTaredas)
+
+const tarefaRealizada = prompt("Digite o índice de uma tarefa já realizada hoje")
+listaDeTaredas.splice(tarefaRealizada, 1)
+console.log(listaDeTaredas)*/
