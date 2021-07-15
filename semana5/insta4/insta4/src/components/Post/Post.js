969ccc03d17a6e819eb8ca304680aa92dcc88df6
Brenda Simionato <brendaevellyn97@gmail.com>
2021-07-15 16:29:15 -0300

    Aula 20 - Renderização de Listas

--- semana5/insta4/insta4/src/components/Post/Post.js
+++ semana5/insta4/insta4/src/components/Post/Post.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import {IconeComContador} from '../IconeComContador/IconeComContador'
+import { IconeComContador } from '../IconeComContador/IconeComContador'
 
 import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
-import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import { SecaoComentario } from '../SecaoComentario/SecaoComentario'
 
 const PostContainer = styled.div`
   border: 1px solid gray;
@@ -42,17 +42,70 @@ const PostPhoto = styled.img`
 
 class Post extends React.Component {
   state = {
+
+    posts: [
+      {
+        nomeUsuario: 'Paula',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: 'Brenda',
+        fotoUsuario: 'https://encrypted-tbn0.gstatic.com/images?q:tbn:ANd9GcTa2kGS59IAv4Dmk3yB8JxFTGC3b0O7cdBApg&usqp:CAU',
+        fotoPost: 'https://blog.pescagerais.com.br/wp-content/uploads/2020/05/paisagens-do-rio-suia-micu-pescaria-2017-6.jpg'
+
+      },
+      {
+        nomeUsuario: 'Andrezza',
+        fotoUsuario: 'https://www.papeiseparede.com.br/7500-large_default/papel-de-parede-paisagem-natural-cachoeira.jpg',
+        fotoPost: 'https://static.significados.com.br/foto/paisagem-natural-cke.jpg'
+      }
+    ],
+
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: "",
+
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
     numeroComentarios: 0
   }
 
+  adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+
+    };
+
+    const novosPost = [... this.state.posts, novoPost];
+    this.setState({ posts: novosPost });
+
+  };
+
+  onChangeInputNomeUsuario = (event) => {
+    this.setState({ valorInputNomeUsuario: event.target.value });
+
+  };
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({ valorInputFotoUsuario: event.target.value });
+
+  };
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({ valorInputFotoPost: event.target.value });
+
+  };
+
+
   onClickCurtida = () => {
-    
+
     let numeroCurtidasAtualizado = this.state.numeroCurtidas
-    
-    if(!this.state.curtido) {
+
+    if (!this.state.curtido) {
       numeroCurtidasAtualizado = numeroCurtidasAtualizado + 1
     } else {
       numeroCurtidasAtualizado = numeroCurtidasAtualizado - 1
@@ -80,7 +133,7 @@ class Post extends React.Component {
   render() {
     let iconeCurtida
 
-    if(this.state.curtido) {
+    if (this.state.curtido) {
       iconeCurtida = iconeCoracaoPreto
     } else {
       iconeCurtida = iconeCoracaoBranco
@@ -88,33 +141,43 @@ class Post extends React.Component {
 
     let componenteComentario
 
-    if(this.state.comentando) {
-      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    if (this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario} />
     }
 
-    return <PostContainer>
-      <PostHeader>
-        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
-        <p>{this.props.nomeUsuario}</p>
-      </PostHeader>
+    return this.state.posts.map((post) => {
 
-      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+      return (
+        
+          <PostContainer>
+            <PostHeader>
+              <UserPhoto src={post.fotoUsuario} alt={'Imagem do usuario'} />
+              <p>{post.nomeUsuario}</p>
+            </PostHeader>
 
-      <PostFooter>
-        <IconeComContador
-          icone={iconeCurtida}
-          onClickIcone={this.onClickCurtida}
-          valorContador={this.state.numeroCurtidas}
-        />
+            <PostPhoto src={post.fotoPost} alt={'Imagem do post'} />
 
-        <IconeComContador
-          icone={iconeComentario}
-          onClickIcone={this.onClickComentario}
-          valorContador={this.state.numeroComentarios}
-        />
-      </PostFooter>
-      {componenteComentario}
-    </PostContainer>
+            <PostFooter>
+              <IconeComContador
+                icone={iconeCurtida}
+                onClickIcone={this.onClickCurtida}
+                valorContador={this.state.numeroCurtidas}
+              />
+
+              <IconeComContador
+                icone={iconeComentario}
+                onClickIcone={this.onClickComentario}
+                valorContador={this.state.numeroComentarios}
+              />
+            </PostFooter>
+            {componenteComentario}
+          </PostContainer>
+    
+      );
+
+    });
+
+
   }
 }
 
