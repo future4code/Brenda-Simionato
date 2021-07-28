5a40ddcf7462fafd8e2ceff1170016f4cb7a56b7
Brenda Simionato <brendaevellyn97@gmail.com>
2021-07-28 14:55:16 -0300

    Adicionado desafio e estilizado

--- semana7/integracoes-de-api/labenusers/src/App.js
+++ semana7/integracoes-de-api/labenusers/src/App.js
@@ -4,6 +4,10 @@ import TelaDeCadastro from "./components/TelaDeCadastro"
 import TelaListaDeUsuarios from "./components/TelaListaDeUsuarios"
 
 const MainContainer = styled.div`
+display: inline-block;
+margin: 10px;
+padding: 10px;
+align-items: center;
 `
 
 
