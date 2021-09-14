8121d9d27a0d15176b9be39ae4f540a8358851a4
brendasimionato <brendaevellyn97@gmail.com>
2021-09-14 13:25:28 -0300

    Inciado home page

--- semana14/case-frontend/src/hooks/HomePage.js
+++ semana14/case-frontend/src/hooks/HomePage.js
@@ -1,12 +1,23 @@
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Card } from "react-bootstrap";
 import React from "react";
+import { imagesURL } from "../constants/url";
 
 
 export default function HomePage() {
 
+    const getImages = useRequestData([], `${imagesURL}`)
 
+    const renderCard = getImages.map((card) => {
+        return (
+            <Card>
+                <img src={card.image} alt={"Imagem da carta"} />
+            </Card>
+        )
+    })
     return (
         <Container>
+
+            {renderCard}
 
             <Button variant="outline-success">Iniciar Jogo</Button>
 
