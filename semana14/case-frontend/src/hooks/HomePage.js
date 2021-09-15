246433fbcc0427b8163067abb2c712f07d2b31c4
brendasimionato <brendaevellyn97@gmail.com>
2021-09-15 10:53:53 -0300

    Inicializado modal

--- semana14/case-frontend/src/hooks/HomePage.js
+++ semana14/case-frontend/src/hooks/HomePage.js
@@ -1,19 +1,28 @@
-import { Container, Button, Card } from "react-bootstrap";
+import { Container, Button, Card, Modal } from "react-bootstrap";
 import { React, useState } from "react";
 import tarot from "../tarot.json"
+import styled from "styled-components";
+
+
+
 
 export default function HomePage() {
 
     const [initGame, setInitGame] = useState(false)
     const [indexChooseCard, setIndexChooseCard] = useState(undefined)
-    const [showDescriptionModal, setShowDescriptionModal] = useState('')
+    //const [showDescriptionModal, setShowDescriptionModal] = useState('')
+    const [show, setShow] = useState(false);
+    const [cardName, setCardName] = useState("")
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const renderOpenTarot = () => {
         return (
             tarot.cards.map((card) => {
                 return (
-                    <Card>
-                        <img src={tarot.imagesUrl + card.image} alt={"Imagem da carta"} />
+                    <Card class=".d-xl-inline-flex"> 
+                        <img  style={{ width: '10rem' }}src={tarot.imagesUrl + card.image} class="rounded mx-auto d-block" alt={"Imagem da carta"} />
                     </Card>
                 )
             })
@@ -28,23 +37,25 @@ export default function HomePage() {
 
                 if (indexChooseCard) {
                     if (indexChooseCard == index) {
+                        setCardName(card.name)
+                        handleShow()
                         return (
-                            <Card>
-                                <img src={tarot.imagesUrl + card.image} alt={"Imagem da carta"} />
+                            <Card style={{ width: '10rem' }} class="card">
+                                <img src={tarot.imagesUrl + card.image} class="card-img-top" alt={"Imagem da carta"} />
                             </Card>
                         )
                     } else {
                         return (
-                            <Card>
-                                <img src={tarot.imageBackCard} alt={"Imagem da carta"} />
+                            <Card style={{ width: '10rem' }} class="card">
+                                <img src={tarot.imageBackCard} class="card-img-top" alt={"Imagem da carta"} />
                             </Card>
                         )
                     }
 
                 } else {
                     return (
-                        <Card>
-                            <img onClick={() => setIndexChooseCard(index)} src={tarot.imageBackCard} alt={"Imagem da carta"} />
+                        <Card class="card">
+                            <img onClick={() => setIndexChooseCard(index)} class="card-img-top" src={tarot.imageBackCard} alt={"Imagem da carta"} />
                         </Card>
                     )
                 }
@@ -61,15 +72,29 @@ export default function HomePage() {
         }
     }
 
-    // const renderDescription = () => {
-       
-    // }
+    
 
     return (
-        <Container>
+        <Container style={{ width: '12rem' }} class="card-group">
 
             <Button onClick={() => setInitGame(true)} variant="outline-success">Iniciar Jogo</Button>
             {render()}
+            {/* <>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>{cardName}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </> */}
 
         </Container>
     )
