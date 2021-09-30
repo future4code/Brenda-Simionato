50478dde32cf2f59ec86fc6924b73aeb16fd74ba
brendasimionato <brendaevellyn97@gmail.com>
2021-09-30 14:26:04 -0300

    Aula 48

---
+++ semana16/relacoes-em-sql/src/index.ts
@@ -0,0 +1,2 @@
+//A chave estrangeira, ou foreign key, é um conceito ligeiramente diferente. Ela não diz respeito, especificamente, a uma tabela, mas sim a um relacionamento entre tabelas. De forma sucinta, a chave estrangeira é uma referência em uma tabela a uma chave primária de outra tabela.
+
